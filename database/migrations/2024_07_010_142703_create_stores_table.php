eca6aa8d4e30482f721638ba744e75e61837b646
NouraMohamed123 <nouramohamed091@gmail.com>
2024-07-09 15:18:04 +0300

    add tables subscription

--- database/migrations/2024_07_010_142703_create_stores_table.php
+++ database/migrations/2024_07_010_142703_create_stores_table.php
@@ -20,12 +20,10 @@ return new class extends Migration
             $table->text('description_en')->nullable();
             $table->text('description_ar')->nullable();
             $table->string('image')->nullable();
-
             $table->foreignId('admin_id')->references('id')->on('admins');
             $table->foreignId('category_id')->references('id')->on('categories');
             $table->foreignId('area_id')->references('id')->on('areas');
-
-
+            $table->foreignId('subscription_id')->references('id')->on('subscriptions');
             $table->boolean('status')->default(true);
             $table->json('boundaries')->nullable();
             $table->string('lat')->nullable();
