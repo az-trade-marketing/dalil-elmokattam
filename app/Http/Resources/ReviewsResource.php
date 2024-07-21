3d12b01c1bfa7ddb1577d8ac2f04811dd6509f70
NouraMohamed123 <nouramohamed091@gmail.com>
2024-07-21 15:34:40 +0300

    يي

--- app/Http/Resources/ReviewsResource.php
+++ app/Http/Resources/ReviewsResource.php
@@ -14,16 +14,16 @@ class ReviewsResource extends JsonResource
      */
     public function toArray($request)
     {
-       return  [
-            'id'=>$this->id,
-           'customer' => [
-                'firstname' => $this->user?->firstname ?? '',
-                'lastname' => $this->user?->lastname ?? '',
-                'photo' => $this->user?->photo ? asset('images/' . $this->user->photo) : '',
-            ],
-            'descriptions'=>$this->descriptions,
-            'rating'=>$this->rating,
-            'created_at'=>$this->created_at
+        return  [
+            'id' => $this->id,
+
+            'firstname' => $this->user?->firstname ?? '',
+            'lastname' => $this->user?->lastname ?? '',
+            'photo' => $this->user?->photo ? asset('images/' . $this->user->photo) : '',
+
+            'message' => $this->message,
+            'rating' => $this->rating,
+            'created_at' => $this->created_at
         ];
     }
 }
